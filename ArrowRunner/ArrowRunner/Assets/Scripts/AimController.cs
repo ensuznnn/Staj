68c125524cef39e8225053dd9b7c889a6ef2378a
ensuznnn <72605380+ensuznnn@users.noreply.github.com>
2021-08-13 11:06:52 +0300

    6

--- ArrowRunner/ArrowRunner/Assets/Scripts/AimController.cs
+++ ArrowRunner/ArrowRunner/Assets/Scripts/AimController.cs
@@ -33,6 +33,40 @@ public class AimController : MonoBehaviour
         {
             followTransform.transform.rotation *= Quaternion.AngleAxis(1, Vector3.right);
         }
+        var angles = followTransform.transform.localEulerAngles;
+        angles.z = 0;
+        var angleX = followTransform.transform.localEulerAngles.x;
+        var angleY = followTransform.transform.localEulerAngles.y;
+        //Clamp the Up/Down rotation
+        if (angleX > 180 && angleX < 340 )
+        {
+            angles.x = 340;
+
+            if (angleX < 180 && angleX > 40)
+            {
+                angles.y = 340;
+            }
+            else if(angleY < 180 && angleY > 40)
+            {
+                angles.y = 40;
+            }
+           
+        }
+        else if (angleX < 180 && angleX > 40 )
+        {
+            angles.x = 40;
+            if (angleX < 180 && angleX > 40)
+            {
+                angles.y = 340;
+            }
+            else if (angleY < 180 && angleY > 40)
+            {
+                angles.y = 40;
+            }
+        }
+   
+
+            followTransform.transform.localEulerAngles = angles;
     }
 
 
